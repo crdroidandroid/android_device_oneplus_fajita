bbcd4b36db1d82637e5342c7af7db458e2317f71
firebird11 <hbgassel@gmail.com>
2020-06-05 14:21:48 +0200

    Fajita: FOD animations
    Change-Id: I9694cc6eabe4992bf2f284bed374608ce34f62de

--- device.mk
+++ device.mk
@@ -47,6 +47,9 @@ PRODUCT_PACKAGES += \
     vendor.oneplus.fingerprint.extension@1.0 \
     vendor.oneplus.hardware.display@1.0
 
+# FOD
+EXTRA_FOD_ANIMATIONS := true
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     device/oneplus/common
