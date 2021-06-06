c0395e46aab1c2b96891a3a383b5eb6b82ac7aab
Terminator-J <terminator_j@hotbutteredmonkey.com>
2021-06-05 22:44:42 -0700

    fajita: fod: Enable UDFPS customization options

--- device.mk
+++ device.mk
@@ -47,6 +47,10 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.oneplus \
     vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 
+# UDFPS
+TARGET_HAS_UDFPS := true
+EXTRA_UDFPS_ANIMATIONS := true
+
 # Inherit from oneplus sdm845-common
 $(call inherit-product, device/oneplus/sdm845-common/common.mk)
 
