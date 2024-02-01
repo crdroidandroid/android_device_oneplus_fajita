8c2a36ab3f3d483449e383039e7d206fc773db30
Terminator-J <terminator_j@hotbutteredmonkey.com>
2024-01-31 23:30:34 -0800

    Revert "fajita: ih8sn: Configure ih8sn"
    This reverts commit 45f75b89d3481a7cd4fbf0e60d95d516c30ec10c.
    
    I'm done fighting with Play Integrity.
    Fuck Google.

--- device.mk
+++ device.mk
@@ -50,10 +50,6 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.oneplus \
     vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 
-# ih8sn
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/ih8sn.conf.fajita:$(TARGET_COPY_OUT_SYSTEM)/etc/ih8sn.conf
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
